b2e10603a1624e943915fd7f35ad4eb9bf1e172b
Azazel009 <47827330+Azazel009@users.noreply.github.com>
2019-11-29 03:14:48 +0500

    latest

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-const path =require('path');
+const path = require('path');
 const http = require('http');
 const fs = require('fs');
-const express=require('express');
+const express = require('express');
 const exphbs = require('express-handlebars');
 const firebase = require("firebase/app");
 require("firebase/firestore");
 require("firebase/auth");
-const app= express();
+const app = express();
+const parser = require("body-parser");
 
 const firebaseConfig = {
-     apiKey: "AIzaSyCtCT7Et6ukWw6Alok-PYzWg3ZK7s9x9pk",
-     authDomain: "project-dff1f.firebaseapp.com",
-     databaseURL: "https://project-dff1f.firebaseio.com",
-     projectId: "project-dff1f",
-     storageBucket: "project-dff1f.appspot.com",
-     messagingSenderId: "469353822648",
-     appId: "1:469353822648:web:b2622b46eacdc56232b74d"
+  apiKey: "AIzaSyCtCT7Et6ukWw6Alok-PYzWg3ZK7s9x9pk",
+  authDomain: "project-dff1f.firebaseapp.com",
+  databaseURL: "https://project-dff1f.firebaseio.com",
+  projectId: "project-dff1f",
+  storageBucket: "project-dff1f.appspot.com",
+  messagingSenderId: "469353822648",
+  appId: "1:469353822648:web:b2622b46eacdc56232b74d"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -26,102 +27,140 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/css",express.static(path.join(__dirname,'views/css')));
-app.use("/assets",express.static(path.join(__dirname,'views/assets')));
-app.use("/bootstrap-4",express.static(path.join(__dirname,'views/bootstrap-4')));
-app.use("/fontawesome",express.static(path.join(__dirname,'views/fontawesome')));
-app.use("/fonts",express.static(path.join(__dirname,'views/fonts')));
-app.use("/images",express.static(path.join(__dirname,'views/images')));
-app.use("/jquery",express.static(path.join(__dirname,'views/jquery')));
-app.use("/js",express.static(path.join(__dirname,'views/js')));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 
-     let venueRef = db.collection('venue');
-     let Allvenues = venueRef.get()
-       .then(snapshot => {
-         snapshot.forEach(doc => {
+
+  let venueRef = db.collection('venue');
+  let Allvenues = venueRef.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        if (flag == 0) {
           venues.push(doc.data());
-         
-         });
-       }).then(function() {
-          console.log(venues);
-          res.render('home',{venues})
-       }
-          )
-       .catch(err => {
-         console.log('Error getting documents', err);
-       });
-            
-});
-
-app.get('/ContactUs',(req,res)=>{
-
-                flag=1;
-               res.render('ContactUs')
-});
-
-app.get('/venu/:name',(req,res)=>{
-
-     let cityRef = db.collection('venue').doc(req.params.name);
-let getDoc = cityRef.get()
-  .then(doc => {
-    if (!doc.exists) {
-      console.log('No such document!');
-    } else {
-      console.log('Document data:', doc.data());
-      venue=doc.data()
-      res.render('venu',{venue})
+        }
+      });
+    }).then(function () {
+      // console.log(venues);
+      flag = 1;
+      res.render('home', { venues })
     }
-  })
-  .catch(err => {
-    console.log('Error getting document', err);
-  });
-      res.render('venu',{venue})
+    )
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
 
 });
 
+app.get('/ContactUs', (req, res) => {
 
-app.get('/selectTheme/:name',(req,res)=>{
+  flag = 1;
+  res.render('ContactUs')
+});
 
-      res.redirect('/selectTheme')
 
+app.get('/selectTheme/:name', (req, res) => {
+
+  let ThemesRef = db.collection('venue').doc(req.params.name);
+  let getDoc = ThemesRef.get()
+    .then(doc => {
+      
    
+        venue = doc.data();
+ 
+        for (var i = 0, len = venue.Themes.length; i < len; i++) {             
+        let ThemeRef = db.collection('themes').doc(venue.Themes[i]);
+        let getDoc = ThemeRef.get()
+          .then(doc => {
+          
+              Themes.push(doc.data())  
+
+          })     
+          .catch(err => {
+            console.log('Error getting document', err);
+          });
+        }
+
+    
+      
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    }).then(doc => {
+      console.log(Themes);
+      res.redirect('/selectTheme')
+  }
+  );
+
+})
+app.get('/selectTheme', (req, res) => {
+
+
+  res.render('selectTheme',{venue,Themes})
 });
-app.get('/selectTheme',(req,res)=>{
 
-     res.render('selectTheme')
 
-  
+app.get('/themeSelected/:name', (req, res) => {
+
+  let ThemeRef = db.collection('themes').doc(req.params.name);
+  let getDoc = ThemeRef.get()
+    .then(doc => {
+
+      theme = doc.data();
+      console.log(doc.data());
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    }).then(doc => {
+      let ThemeRef = db.collection('users').doc(venue.Owner);
+      let getDoc = ThemeRef.get()
+        .then(doc => {
+
+          user = doc.data();
+          res.redirect('/themeSelected')
+        })
+        .catch(err => {
+          console.log('Error getting document', err);
+        })
+    });
+
 });
 
-app.get('/themeSelected/:name',(req,res)=>{
 
-       res.render('themeSelected')
+
+
+app.get('/themeSelected/', (req, res) => {
+  console.log(venue);
+  console.log(user);
+  console.log(theme);
+  res.render('themeSelected',{venue,user,theme})
 });
-app.get('/payment',(req,res)=>{
+
+app.get('/payment', (req, res) => {
 
 
-     res.render('payment')
+  res.render('payment')
 });
 
-app.use('/',require('./routes/Authentication'))
+app.use('/', require('./routes/Authentication'))
 
- //static folder
-
-
+//static folder
 
 
-const PORT=process.env.PORT || 1000;
-app.listen(PORT, ()=>console.log("server started on Port 1000"));
+app.use(express.static(path.join(__dirname, 'views')));
+
+const PORT = process.env.PORT || 1000;
+app.listen(PORT, () => console.log("server started on Port 1000"));
 
 
-var flag=0;
-var names='Tumbling';
+var flag = 0;
+var names = 'Tumbling';
 
 
-var venues=[];
+const venues = [];
 
-var venue={};
+var venue = {};
+var Themes=[];
+var theme={};
+var user={};
--- routes/Authentication.js
+++ routes/Authentication.js
@@ -14,7 +14,7 @@ var db = firebase.firestore();
 router.post('/signin',(req,res)=>{
 
 
-    firebase.auth().signInWithEmailAndPassword(req.body.email,req.body.password).then((cred)=>{
+    firebase.auth().signInWithEmailAndPassword(req.body.Email,req.body.Passowrd).then((cred)=>{
     console.log(cred);
    }).catch(err=>{
         console.log(err);
@@ -28,9 +28,22 @@ router.post('/signin',(req,res)=>{
 
 router.post('/signup',(req,res)=>{
 
-
+    let docRef = db.collection('users').doc(req.body.Name);
+    let setAda = docRef.set({
+        Name: req.body.Name,
+        Passowrd: req.body.Passowrd,
+        PaypalID: req.body.PaypalID,
+        BankRouting: req.body.BankRouting,
+        CheckingAcct: req.body.CheckingAcct,
+        BusinessName: req.body.BusinessName,
+        Address: req.body.Address,
+        City: req.body.City,
+        Phone: req.body.Phone,
+        Email: req.body.Email,
+        Website: req.body.Website
+    });
 	var auth = firebase.auth();
-    auth.createUserWithEmailAndPassword(req.body.email,req.body.password).then((cred)=>{
+    auth.createUserWithEmailAndPassword(req.body.Email,req.body.Passowrd).then((cred)=>{
     console.log(cred);
     }).catch(err=>{
      console.log(err);
